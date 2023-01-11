148b8f63717da113eca6bacd4639acc5bf05b77c
Natan <mouranatan933@gmail.com>
2023-01-11 16:07:10 -0300

    fgd

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-// var restart = false;
-// var gravidade= .6;
-// var correr = 7.2;
-// var jump = -11.5;
+var restart = false;
+var gravidade= .6;
+var correr = 7.2;
+var jump = -11.5;
 
 
 
-// document.addEventListener('click', () => restart = true);
-// window.onclick = () => flight = jump;
+document.addEventListener('click', () => restart = true);
+window.onclick = () => flight = jump;
