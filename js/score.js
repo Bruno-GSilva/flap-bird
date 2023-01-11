821526cf378ffcb06b3d4520b34fbc6e8b10bb87
LucasDCoelho <darkalp99@gmail.com>
2023-01-11 18:01:18 -0300

    depressão

--- js/score.js
+++ js/score.js
@@ -1,18 +1,22 @@
-import "./index";
 
-let colide = Boolean(colisor.value);
-let score = 0;
-let player = true;
 
-if (player){
+function getBelowElements(el) {
+  const sourceBounding = el.getBoundingClientRect();
+  let belowElements = [];
 
+  for (const currentElement of document.documentElement) {
+      const targetBounding = currentElement.getBoundingClientRect();
+
+      // Se todas as condições forem falsas, é porque está tendo uma sobreposição
+      if (!(sourceBounding.right < targetBounding.left ||
+          sourceBounding.left > targetBounding.right ||
+          sourceBounding.bottom < targetBounding.top ||
+          sourceBounding.top > targetBounding.bottom)) {
+          belowElements = [...belowElements, currentElement]; // Adiciona o elemento atual ao array de elementos sobrepostos
+      }
+  }
+
+  return belowElements; // Retorna o array de elementos sobrepostos
 }
 
-for (let column = 0; column < map.columns; column++) {
-    for (let row = 0; row < map.rows; row++) {
-      const tile = map.getTile(column, row);
-      const x = column * map.tileSize;
-      const y = row * map.tileSize;
-      drawTile(tile, x, y);
-    }
-  }
+console.log(getBelowElements(document.getElementsByClassName("hitbox")[0]));
