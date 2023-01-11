5c533618ddb3d513f605823563f7a26c4fe6ce9c
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-01-11 18:01:49 -0300

    depresao2.0

--- ambiente-testes/parallax.js
+++ ambiente-testes/parallax.js
@@ -1,7 +1,5 @@
-let background = document.querySelector('span')
-
-window.addEventListener('scroll', function(){
-    var obstaculo = window.pageYOffset;
-
-    background.style.backgroundPositionY = -obstaculo * 0.7 + 'px';
+window.addEventListener("scroll", function() {
+  const distance = window.scrollY;
+  document.querySelector(".obstaculo:first-child").style.transform = `translateY(${distance *
+    1}px)`;
 });
