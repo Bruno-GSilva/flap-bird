55945893d60fb8a8a2c78f8b479631548a5d7292
LucasDCoelho <darkalp99@gmail.com>
2023-01-16 10:54:45 -0300

    alteracoes

--- ambiente-testes/parallax.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function() {
-  const distance = window.scrollY;
-  document.querySelector(".obstaculo:first-child").style.transform = `translateY(${distance *
-    1}px)`;
-});
