af91da0d30ca6ac15d1bbf94281c35a5d3583584
LucasDCoelho <94465173+LucasDCoelho@users.noreply.github.com>
2023-01-17 16:50:48 -0300

    adicionado o contador

--- ambiente-testes/score.js
+++ ambiente-testes/score.js
@@ -1,15 +1,20 @@
-const font_score = document.getElementById('font_score');
-const ctx_score = font_score.getContext("2d");
+let score = document.getElementById('text_score');
+// const ctx_score = score.getContext("2d");
+
+let updtScore = 0;
+// ctx_score.font = "bold 49px sans_serif";ctx_score.fillText(`Score`, 100, 100);
+const setCount = setInterval(() => {
+    if(updtScore === 10){
+        clearInterval(setCount)
+    }else{
+        updtScore++;
+    }
+    score.innerText = `Score ${updtScore}`;
+}, 1000)
 
 
-ctx_score.font = "bold 49px sans_serif";
-ctx_score.strokeText(`Score ${setCount()}`, 50, 100);
+
+console.log(setCount);
 
 
-function setCount(){
-    let count = [];
-   console.count(count);
-   return count;
-}
-console.log(setCount());
 
