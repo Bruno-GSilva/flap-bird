aea3508c3af1a8d6e92564a8193180f2188d92d1
LucasDCoelho <darkalp99@gmail.com>
2023-01-18 11:11:43 -0300

    topzera

--- ambiente-testes/score.js
+++ ambiente-testes/score.js
@@ -1,16 +1,19 @@
+// import "./parallaxAnimation.js";
+
 let score = document.getElementById('text_score');
+
 // const ctx_score = score.getContext("2d");
 
 let updtScore = 0;
 // ctx_score.font = "bold 49px sans_serif";ctx_score.fillText(`Score`, 100, 100);
 const setCount = setInterval(() => {
-    if(updtScore === 10){
+    if(updtScore === Infinity){
         clearInterval(setCount)
     }else{
         updtScore++;
     }
     score.innerText = `Score ${updtScore}`;
-}, 1000)
+}, 10)
 
 
 
